5917d10959a0bd4708f8ff22f0de298fd398745c
daniele992 <moranidaniele129@yahoo.it>
2024-08-27 18:14:32 +0200

    Aggiunte stringhe al file colors e image_Strings

--- lib/src/constants/colors.dart
+++ lib/src/constants/colors.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 /* --- LISTS OF COLORS ---*/
 
 const tPrimaryColor = Color(0xFFFFE400);
@@ -9,3 +11,7 @@ const tAccentColor = Color(0xFF001BFF);
 const tCardBgColor = Color(0xFFF7F6F1);
 const tWhiteColor = Color(0xffffffff);
 const tDarkColor = Color(0xFF000000);
+
+const tOnBoardingPage1Color = Colors.white;
+const tOnBoardingPage2Color = Color(0xfffddcdf);
+const tOnBoardingPage3Color = Color(0xffffdcbd);
--- lib/src/constants/image_strings.dart
+++ lib/src/constants/image_strings.dart
@@ -1,4 +1,8 @@
 /* --- APP IMAGE STRINGS --- */
 
-const String tSplashTopIcon = "assets/images/splash_screen_icons.png";
-const String tSplashImage = "assets/images/splash_screen.png";
+const String tSplashTopIcon = "assets/images/splash_images/splash_screen_icons.png";
+const String tSplashImage = "assets/images/splash_images/splash_screen.png";
+
+const String tOnBoardingImage1 = "";
+const String tOnBoardingImage2 = "";
+const String tOnBoardingImage3 = "";
