03f5a077d1d79fc285a662bde3dda4c6af3c3f6c
daniele992 <moranidaniele129@yahoo.it>
2024-08-27 18:12:51 +0200

    Aggiunti file per la gestione on boarding

---
+++ lib/src/features/authentication/view/on_boarding/on_boarding_page_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../../../../constants/sizes.dart';
+import '../../models/model_on_boarding.dart';
+
+class OnBoardingPageWidget extends StatelessWidget {
+  const OnBoardingPageWidget({
+    //super.key,
+    Key? key,
+    required this.model,
+  }) : super(key: key);
+
+  final OnBoardingModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.all(tDefaultSize),
+      color: model.bgColor,
+      child: Column( //const?
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image(image: AssetImage(model.image), height: size.height * 0.45,),
+          Column(
+            children: [
+              Text(model.title, style: Theme.of(context).textTheme.headlineMedium),
+              Text(model.subTitle, textAlign: TextAlign.center),
+            ],
+          ),
+          Text(model.counterText, style: Theme.of(context).textTheme.headlineSmall),
+          const SizedBox(height: 80.0,)
+        ],
+      ),
+    );
+  }
+} // class OnBoardingScreen
---
+++ lib/src/features/authentication/view/on_boarding/on_boarding_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:gestionale_calcio_mvc_flutter/src/constants/image_strings.dart';
+import 'package:gestionale_calcio_mvc_flutter/src/constants/sizes.dart';
+import 'package:gestionale_calcio_mvc_flutter/src/constants/text_strings.dart';
+import 'package:gestionale_calcio_mvc_flutter/src/features/authentication/models/model_on_boarding.dart';
+import 'package:gestionale_calcio_mvc_flutter/src/features/authentication/view/on_boarding/on_boarding_page_widget.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import '../../../../constants/colors.dart';
+import '../../controllers/on_boarding_controller.dart';
+
+class OnBoardingScreen extends StatelessWidget{
+  const OnBoardingScreen({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    final obController = OnBoardingController();
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          LiquidSwipe(
+            pages: obController.pages,
+            liquidController: obController.controller,
+            onPageChangeCallback: obController.onPageChangedCallback,
+            slideIconWidget: const Icon(Icons.arrow_back_ios),
+            enableSideReveal: true,
+          ),
+          Positioned(
+            bottom: 60.0,
+            child: OutlinedButton(
+              onPressed: () => obController.animateToNextSlide(),
+              style: ElevatedButton.styleFrom(
+                side: const BorderSide(color: Colors.black26),
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(20),
+                backgroundColor: Colors.white, //OnPrimary?
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                decoration: const BoxDecoration(
+                  color: tDarkColor, shape: BoxShape.circle
+                ),
+                child: const Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 50,
+            right: 20,
+            child: TextButton(
+                onPressed: () => obController.skip(),
+                child: const Text("Skip", style: TextStyle(color: Colors.grey))
+            ),
+          ),
+          Obx(
+            () => Positioned(
+            bottom: 10,
+            child: AnimatedSmoothIndicator(
+              activeIndex: obController.currentPage.value,
+              count: 3,
+              effect: const WormEffect(
+                activeDotColor: Color(0xff272727),
+                dotHeight: 5.0
+              ),
+            ),
+          ))
+        ],
+      ),
+    );//Closed Scaffold
+  }
+ // Closed Widget
+}
+
