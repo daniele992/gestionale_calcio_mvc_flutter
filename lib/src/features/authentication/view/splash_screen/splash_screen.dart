0dd2ad84d5338e9f70e0d5724a285dfe73528da2
daniele992 <moranidaniele129@yahoo.it>
2024-08-29 15:22:34 +0200

    Creato flutter animation, lezione 8

--- lib/src/features/authentication/view/splash_screen/splash_screen.dart
+++ lib/src/features/authentication/view/splash_screen/splash_screen.dart
@@ -1,38 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:gestionale_calcio_mvc_flutter/src/common_widgets/fade_in_animation/animation_design.dart';
 import 'package:gestionale_calcio_mvc_flutter/src/constants/colors.dart';
 import 'package:gestionale_calcio_mvc_flutter/src/constants/image_strings.dart';
 import 'package:gestionale_calcio_mvc_flutter/src/constants/sizes.dart';
 import 'package:gestionale_calcio_mvc_flutter/src/constants/text_strings.dart';
-import 'package:gestionale_calcio_mvc_flutter/src/features/authentication/controllers/splash_screen_controller.dart';
+import 'package:gestionale_calcio_mvc_flutter/src/common_widgets/fade_in_animation/splash_screen_controller.dart';
 import 'package:gestionale_calcio_mvc_flutter/src/features/authentication/view/welcome/welcome_screen.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 
+import '../../../../common_widgets/fade_in_animation/fade_in_animation_model.dart';
+
 class SplashScreen extends StatelessWidget {
   SplashScreen({Key? key}) : super(key: key);
 
-  final splashController = Get.put(SplashScreenController());
+  //final splashController = Get.put(SplashScreenController());
 
   @override
   Widget build(BuildContext context){
-    splashController.startAnimation();
+    final controller = Get.put(FadeInAnimationController());
+    controller.startSplashAnimation();
+
     return Scaffold(
       body: Stack(
         children: [
-          Obx(() => AnimatedPositioned(
-            duration: const Duration(milliseconds: 1600),
-            top: splashController.animate.value ? 0 : -30,
-            left: splashController.animate.value ? 0 : -30,
-            child: Image(image: AssetImage(tSplashTopIcon)),
-            )
+          TFadeInAnimation(
+            durationInMs: 1600,
+            animate: TAnimatePosition(
+              topAfter: 0, topBefore: -30, leftBefore: -30, leftAfter: 0,
+            ),
+            child: const Image(image: AssetImage(tSplashTopIcon)),
           ),
-          Obx ( () => AnimatedPositioned(
-            duration: const Duration(milliseconds: 1600),
-            top: 80,
-            left: splashController.animate.value ? tDefaultSize : -80,
-            child: AnimatedOpacity(
-              duration: const Duration(milliseconds: 1600),
-              opacity: splashController.animate.value ? 1 : 0,
+          TFadeInAnimation (
+            durationInMs: 2000,
+              animate: TAnimatePosition(topBefore: 80, topAfter: 80, leftAfter: tDefaultSize, leftBefore: -80),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -46,25 +47,16 @@ class SplashScreen extends StatelessWidget {
                    ), //textTheme.headline2?
                   ],
                 ),
-              ),
-            )
-          ),
-           Obx ( () => AnimatedPositioned(duration: const Duration(milliseconds: 2400),
-            bottom: splashController.animate.value ? 100 : 0,
-            child: AnimatedOpacity(
-              duration: const Duration(milliseconds: 2000),
-              opacity: splashController.animate.value ? 1 : 0,
-              child: Image(image: AssetImage(tSplashImage)),
-              )
-             ),
            ),
-          Obx( () => AnimatedPositioned(duration: const Duration(milliseconds: 2400),
-            bottom: splashController.animate.value ? 60 : 0,
-            right: tDefaultSize,
-             child: AnimatedOpacity(
-               duration: const Duration(milliseconds: 2000),
-               opacity: splashController.animate.value ? 1 : 0,
-                 child: Container(
+           TFadeInAnimation(
+             durationInMs: 2400,
+             animate: TAnimatePosition(bottomBefore: 0, bottomAfter: 100),
+             child: const Image(image: AssetImage(tSplashImage)),
+           ),
+           TFadeInAnimation(
+             durationInMs: 2400,
+               animate: TAnimatePosition(bottomBefore: 0, bottomAfter: 60, rightBefore: tDefaultSize, rightAfter: tDefaultSize),
+               child: Container(
                    width: tSplashContainerSize,
                    height: tSplashContainerSize,
                    decoration: BoxDecoration(
@@ -72,14 +64,10 @@ class SplashScreen extends StatelessWidget {
                      color: tPrimaryColor,
                    ),
                  )
-               ),
-             ),
-          )
+           ),
         ], //Children
       ),
     );
   }
+}
 
-
-
-} //Closed class _SplashScreenState
--- lib/src/features/authentication/view/welcome/welcome_screen.dart
+++ lib/src/features/authentication/view/welcome/welcome_screen.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:gestionale_calcio_mvc_flutter/src/common_widgets/fade_in_animation/animation_design.dart';
 import 'package:gestionale_calcio_mvc_flutter/src/constants/colors.dart';
 import 'package:gestionale_calcio_mvc_flutter/src/constants/image_strings.dart';
 import 'package:gestionale_calcio_mvc_flutter/src/constants/sizes.dart';
 import 'package:gestionale_calcio_mvc_flutter/src/constants/text_strings.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../../../../common_widgets/fade_in_animation/fade_in_animation_model.dart';
+import '../../../../common_widgets/fade_in_animation/splash_screen_controller.dart';
 
 class WelcomeScreen extends StatelessWidget{
   const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context){
+
+    final controller = Get.put(FadeInAnimationController());
+    controller.startAnimation();
+
     var mediaQuery = MediaQuery.of(context);
     var height = MediaQuery.of(context).size.height;
     var brightness = mediaQuery.platformBrightness;
@@ -16,45 +26,63 @@ class WelcomeScreen extends StatelessWidget{
 
     return Scaffold(
       backgroundColor: isDarkMode ? tSecondaryColor: tPrimaryColor,
-      body: Container(
-        padding: EdgeInsets.all(tDefaultSize),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-             Image(
-              image: AssetImage(tWelcomeScreenImage),
-              height: height * 0.6,
-            ), //const?
-             Column( //const?
-              children: [
-                Text(
-                  tWelcomeTitle,
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ), //headLine3?
-                Text(
-                  tWelcomeSubTitle,
-                  style: Theme.of(context).textTheme.bodyLarge,
-                  textAlign: TextAlign.center,
-                ), //bodyText1?
-              ],
+      body: Stack(
+        children: [
+          TFadeInAnimation(
+            durationInMs: 1200,
+            animate: TAnimatePosition(
+                bottomAfter: 0,
+                bottomBefore: -100,
+                leftBefore: 0,
+                leftAfter: 0,
+                topAfter: 0,
+                topBefore: 0,
+                rightAfter: 0,
+                rightBefore: 0,
             ),
-            Row(
-              children: [
-                Expanded(
-                    child: OutlinedButton(
-                        onPressed: (){},
-                        child: Text(tLogin.toUpperCase())),),
-                const SizedBox(
-                  width: 10.0,
-                ),
-                Expanded(
-                    child: ElevatedButton(
-                        onPressed: (){},
-                        child: Text(tSignup.toUpperCase())),),
-              ],
-            )
-          ],
-        ),
+            child: Container(
+              padding: EdgeInsets.all(tDefaultSize),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Hero(
+                      tag: 'Welcome-image-tag',
+                      child: Image(
+                          image: const AssetImage(tWelcomeScreenImage),
+                        height: height * 0.6)), //const?
+                   Column( //const?
+                    children: [
+                      Text(
+                        tWelcomeTitle,
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ), //headLine3?
+                      Text(
+                        tWelcomeSubTitle,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                        textAlign: TextAlign.center,
+                      ), //bodyText1?
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: OutlinedButton(
+                              onPressed: (){},
+                              child: Text(tLogin.toUpperCase())),),
+                      const SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                          child: ElevatedButton(
+                              onPressed: (){},
+                              child: Text(tSignup.toUpperCase())),),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     ); //Scaffold
 
