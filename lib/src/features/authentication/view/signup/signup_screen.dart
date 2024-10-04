b0e8ec286a0c47655d152f2ca45abe16ac4e7eed
daniele992 <moranidaniele129@yahoo.it>
2024-10-04 17:47:20 +0200

    Lezione 18, 10.2 ma rivedere e fare i primi 5 minuti

--- lib/src/features/authentication/view/signup/signup_screen.dart
+++ lib/src/features/authentication/view/signup/signup_screen.dart
@@ -14,6 +14,7 @@ class SignUpScreen extends StatelessWidget{
   Widget build(BuildContext context)  {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(),
         body: SingleChildScrollView(
          child: Container(
            padding: const EdgeInsets.all(tDefaultSize),
@@ -23,7 +24,7 @@ class SignUpScreen extends StatelessWidget{
                  image: tWelcomeScreenImage,
                  title: tSignUpTitle,
                  subTitle: tSignUpSubTitle,
-                   imageHeight: 0.15,
+                   imageHeight: 0.1,
                ),
                SignUpFormWidget(),
                SignUpFooterWidget(),
