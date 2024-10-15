6e5edc01a4fa796a9cef7dde0b2b19f12b03590f
daniele992 <moranidaniele129@yahoo.it>
2024-10-15 17:41:42 +0200

    modificato on boarding controller

--- lib/src/features/authentication/controllers/on_boarding_controller.dart
+++ lib/src/features/authentication/controllers/on_boarding_controller.dart
@@ -5,49 +5,60 @@ import '../../../constants/image_strings.dart';
 import '../../../constants/text_strings.dart';
 import '../models/model_on_boarding.dart';
 import '../view/on_boarding/on_boarding_page_widget.dart';
+import '../view/welcome/welcome_screen.dart';
 
 class OnBoardingController extends GetxController {
 
+  //Variables
+  final userStorage = GetStorage(); // Use for local Storage
   final controller = LiquidController();
   RxInt currentPage = 0.obs;
+
+  //Functions to trigger Skip, Next and onPageChange Events
+  skip() => controller.jumpToPage(page: 2);
+
+  animateToNextSlide() => controller.animateToPage(page: controller.currentPage + 1);
+
+  animateToNextSlideWithLocalStorage() {
+    if(controller.currentPage == 2) {
+      userStorage.write('isFirstTime', false);
+      print(userStorage.read('isFirstTime'));
+      Get.offAll(() => const WelcomeScreen());
+    } else {
+      controller.animateToPage(page: controller.currentPage + 1);
+    }
+  }
+
+  onPageChangedCallback(int activePageIndex) => currentPage.value = activePageIndex;
+
+  //Three Onboarding Pages
   final pages = [
     OnBoardingPageWidget(
       model: OnBoardingModel(
-          image: tOnBoardingImage1,
-          title: tOnBoardingTitle1,
-          subTitle: tOnBoardingSubTitle1,
-          counterText: tOnBoardingCounter1,
-          //height: size.height,
-          bgColor: tOnBoardingPage1Color
+        image: tOnBoardingImage1,
+        title: tOnBoardingTitle1,
+        subTitle: tOnBoardingSubTitle1,
+        counterText: tOnBoardingCounter1,
+        bgColor: tOnBoardingPage1Color,
       ),
     ),
     OnBoardingPageWidget(
       model: OnBoardingModel(
-          image: tOnBoardingImage2,
-          title: tOnBoardingTitle2,
-          subTitle: tOnBoardingSubTitle2,
-          counterText: tOnBoardingCounter2,
-          //height: size.height,
-          bgColor: tOnBoardingPage2Color
+        image: tOnBoardingImage2,
+        title: tOnBoardingTitle2,
+        subTitle: tOnBoardingSubTitle2,
+        counterText: tOnBoardingCounter2,
+        bgColor: tOnBoardingPage2Color,
       ),
     ),
     OnBoardingPageWidget(
-      model: OnBoardingModel(
-          image: tOnBoardingImage3,
-          title: tOnBoardingTitle3,
-          subTitle: tOnBoardingSubTitle3,
-          counterText: tOnBoardingCounter3,
-          //height: size.height,
-          bgColor: tOnBoardingPage3Color
+        model: OnBoardingModel(
+        image: tOnBoardingImage3,
+        title: tOnBoardingTitle3,
+        subTitle: tOnBoardingSubTitle3,
+        counterText: tOnBoardingCounter3,
+        bgColor: tOnBoardingPage3Color,
       ),
-    )
+    ),
   ];
-
-  void onPageChangedCallback(int activePageIndex) => currentPage.value = activePageIndex;
-  skip() => controller.jumpToPage(page: 2);
-  animateToNextSlide() {
-    int nextPage = controller.currentPage + 1;
-    controller.animateToPage(page: nextPage);
-  }
-
 }
