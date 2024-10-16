776faac80a0a499a1a753bf09064763770d651c3
daniele992 <moranidaniele129@yahoo.it>
2024-10-16 17:21:03 +0200

    modificato login form

--- lib/src/features/authentication/view/login/widgets/login_form_widget.dart
+++ lib/src/features/authentication/view/login/widgets/login_form_widget.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import '../../../../../common_widgets/buttons/primary_button.dart';
 import '../../../../../constants/sizes.dart';
 import '../../../../../constants/text_strings.dart';
+import '../../../../../utils/helper/helper_controller.dart';
+import '../../../controllers/login_controller.dart';
 import '../../forget_password/forget_password_options/forget_password_model_bottom_sheet.dart';
 
 class LoginFormWidget extends StatelessWidget {
@@ -10,59 +16,71 @@ class LoginFormWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: tFormHeight - 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.person_outline_outlined),
-                  labelText: tEmail,
-                  hintText: tEmail,
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: tFormHeight - 20),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.fingerprint),
-                    labelText: tPassword,
-                    hintText: tPassword,
-                    border: OutlineInputBorder(),
-                    suffixIcon: IconButton(
-                      onPressed: null,
-                      icon: Icon(Icons.remove_red_eye_sharp),
-                    )
-                ),
-              ),
-              const SizedBox(height: tFormHeight - 20),
+    final controller = Get.put(LoginController());
+    return Container(
+     padding: const EdgeInsets.symmetric(vertical: tFormHeight),
+     child: Form(
+       key: controller.loginFormKey,
+       child: Column(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           /// -- Email Field
+           TextFormField(
+             validator: Helper.validateEmail,
+             controller: controller.email,
+             decoration: const InputDecoration(prefixIcon: Icon(LineAwesomeIcons.user), labelText: tEmail,hintText: tEmail),
+           ),
+           const SizedBox(height: tFormHeight - 20),
 
-              // -- FORGET PASSWORD BTN
-              Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                      onPressed: (){
-                        ForgetPasswordScreen.buildShowModalBottomSheet(context);
-                      },
-                      child: const Text(tForgetPassword))
-              ),
+           /// -- Password Field
+           Obx(
+               () => TextFormField(
+                 controller: controller.password,
+                 validator: (value) {
+                   if(value!.isEmpty) return 'Enter your password';
+                   return null;
+                 },
+                 obscureText: controller.showPassword.value ? false : true,
+                 decoration: InputDecoration(
+                   prefixIcon: const Icon(Icons.fingerprint),
+                   labelText: tPassword,
+                   hintText: tPassword,
+                   suffixIcon: IconButton(
+                       icon: controller.showPassword.value
+                           ? const Icon(LineAwesomeIcons.eye)
+                           : const Icon(LineAwesomeIcons.eye_slash),
+                     onPressed: () => controller.showPassword.value = !controller.showPassword.value,
+                   ),
+                 ),
+               ),
+           ),
+           const SizedBox(height: tFormHeight - 20),
 
-              // -- LOGIN BTN
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: (){},
-                  child: Text(tLogin.toUpperCase()),
-                ),
-              ),
-            ],
-          ),
-        )
+           /// -- FORGET PASSWORD BTN
+           Align(
+             alignment: Alignment.centerRight,
+             child: TextButton(
+               onPressed: () => ForgetPasswordScreen.buildShowModalBottomSheet(context),
+               child: const Text(tForgetPassword),
+             ),
+           ),
+
+           /// -- LOGIN BTN
+           Obx(
+               () => TPrimaryButton(
+                 isLoading: controller.isLoading.value ? true : false,
+                 text: tLogin.tr,
+                   onPressed: controller.isFacebookLoading.value || controller.isGoogleLoading.value
+                       ? () {}
+                       : controller.isLoading.value
+                          ? () {}
+                          : () => controller.login(),
+               ),
+           ),
+         ],
+       ),
+     ),
     );
   }
-
-
 }
 
