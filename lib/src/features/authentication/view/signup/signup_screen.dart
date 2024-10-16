ed6169aa2721c717f152482c161f746e21ac5ead
daniele992 <moranidaniele129@yahoo.it>
2024-10-16 17:36:27 +0200

    modificato signup_screen.dart

--- lib/src/features/authentication/view/signup/signup_screen.dart
+++ lib/src/features/authentication/view/signup/signup_screen.dart
@@ -1,11 +1,13 @@
 import  "package:flutter/material.dart";
+import "package:gestionale_calcio_mvc_flutter/src/common_widgets/form/form_divider_widget.dart";
 import "package:gestionale_calcio_mvc_flutter/src/common_widgets/form/form_header_widget.dart";
+import "package:gestionale_calcio_mvc_flutter/src/common_widgets/form/social_footer.dart";
 import "package:gestionale_calcio_mvc_flutter/src/constants/image_strings.dart";
 import "package:gestionale_calcio_mvc_flutter/src/constants/sizes.dart";
 import "package:gestionale_calcio_mvc_flutter/src/constants/text_strings.dart";
+import "package:gestionale_calcio_mvc_flutter/src/features/authentication/view/login/login_screen.dart";
 import "package:gestionale_calcio_mvc_flutter/src/features/authentication/view/signup/widgets/signup_forms_widget.dart";
-
-import "../../../../common_widgets/form/form_footer_widget.dart";
+import "package:get/get.dart";
 
 class SignUpScreen extends StatelessWidget{
   const SignUpScreen({super.key});
@@ -14,27 +16,31 @@ class SignUpScreen extends StatelessWidget{
   Widget build(BuildContext context)  {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(),
         body: SingleChildScrollView(
-         child: Container(
-           padding: const EdgeInsets.all(tDefaultSize),
-           child: const Column(
-             children:[
-               FormHeaderWidget(
-                 image: tWelcomeScreenImage,
-                 title: tSignUpTitle,
-                 subTitle: tSignUpSubTitle,
-                   imageHeight: 0.1,
-               ),
-               SignUpFormWidget(),
-               SignUpFooterWidget(),
-             ],
-           ),
-         ),
-        )
+          child: Container(
+            padding: const EdgeInsets.all(tDefaultSpace),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const FormHeaderWidget(
+                  image: tWelcomeScreenImage,
+                  title: tSignUpTitle,
+                  subTitle: tSignUpSubTitle,
+                  imageHeight: 0.1,
+                ),
+                const SignUpFormWidget(),
+                const TFormDividerWidget(),
+                SocialFooter(
+                  text1: tAlreadyHaveAnAccount,
+                  text2: tLogin,
+                  onPressed: () => Get.off(() => const LoginScreen()),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-    ); //Scaffold
+    );
   }
-
 }
 
