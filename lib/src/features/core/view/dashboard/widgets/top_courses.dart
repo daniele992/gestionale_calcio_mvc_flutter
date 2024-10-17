6c1bc21eea6ca069561edbce7691dda4d6687c16
daniele992 <moranidaniele129@yahoo.it>
2024-10-17 15:42:23 +0200

    Fix top_courses.dart

--- lib/src/features/core/view/dashboard/widgets/top_courses.dart
+++ lib/src/features/core/view/dashboard/widgets/top_courses.dart
@@ -3,20 +3,23 @@ import '../../../../../constants/colors.dart';
 import '../../../../../constants/image_strings.dart';
 import '../../../../../constants/sizes.dart';
 import '../../../models/dashboard/categories_model.dart';
+import '../../../models/dashboard/courses_model.dart';
 
 class DashboardTopCourses extends StatelessWidget {
   const DashboardTopCourses({
     super.key,
     required this.txtTheme,
+    required this.isDark,
   });
 
   final TextTheme txtTheme;
+  final bool isDark;
 
   @override
   Widget build(BuildContext context) {
-    final list = DashboardCategoriesModel.list;
+    final list = DashboardTopCoursesModel.list;
     return SizedBox(
-      height: 200,
+      height: 210,
       child: ListView.builder(
         shrinkWrap: true,
         scrollDirection: Axis.horizontal,
@@ -25,11 +28,14 @@ class DashboardTopCourses extends StatelessWidget {
           onTap: list[index].onPress,
           child: SizedBox(
             width: 320,
-            height: 200,
+            height: 210,
             child: Padding(
               padding: const EdgeInsets.only(right: 10, top: 5),
               child: Container(
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: tCardBgColor),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
+                  //For Dark Color
+                  color: isDark ? tSecondaryColor : tCardBgColor,
+                ),
                 padding: const EdgeInsets.all(10),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -40,12 +46,12 @@ class DashboardTopCourses extends StatelessWidget {
                         Flexible(
                             child: Text(
                               list[index].title,
-                              style: txtTheme.headlineMedium, //headLine4
+                              style: txtTheme.headlineMedium,
                               maxLines: 2,
                               overflow: TextOverflow.ellipsis,
-                            )
+                            ),
                         ),
-                        const Flexible(child: Image(image: AssetImage(tTopCourseImage1), height: 110)),
+                        Flexible(child: Image(image: AssetImage(list[index].image), height: 110)),
                       ],
                     ),
                     Row(
@@ -59,8 +65,16 @@ class DashboardTopCourses extends StatelessWidget {
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text("3 Sections", style: txtTheme.headlineMedium, overflow: TextOverflow.ellipsis), //headLine4
-                            Text("Programming Languages", style: txtTheme.bodyMedium, overflow: TextOverflow.ellipsis), //bodyText2
+                            Text(
+                              list[index].heading,
+                              style: txtTheme.headlineMedium,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Text(
+                              list[index].subHeading,
+                              style: txtTheme.bodyMedium,
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ],
                         )
                       ],
@@ -70,182 +84,6 @@ class DashboardTopCourses extends StatelessWidget {
               ),
             ),
           ),
-          /*SizedBox(
-            width: 320,
-            height: 200,
-            child: Container(
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: tCardBgColor),
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                          child: Text(
-                            "Flutter Crash Course",
-                            style: txtTheme.headlineMedium, //headLine4
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          )
-                      ),
-                      const Flexible(child: Image(image: AssetImage(tTopCourseImage1), height: 110)),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(shape: const CircleBorder()),
-                        onPressed: (){},
-                        child: const Icon(Icons.play_arrow),
-                      ),
-                      const SizedBox(width: tDashBoardCardPadding),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("3 Sections", style: txtTheme.headlineMedium, overflow: TextOverflow.ellipsis), //headLine4
-                          Text("Programming Languages", style: txtTheme.bodyMedium, overflow: TextOverflow.ellipsis), //bodyText2
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            width: 320,
-            height: 200,
-            child: Container(
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: tCardBgColor),
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                          child: Text(
-                            "Flutter Crash Course",
-                            style: txtTheme.headlineMedium, //headLine4
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          )
-                      ),
-                      const Flexible(child: Image(image: AssetImage(tTopCourseImage1), height: 110)),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(shape: const CircleBorder()),
-                        onPressed: (){},
-                        child: const Icon(Icons.play_arrow),
-                      ),
-                      const SizedBox(width: tDashBoardCardPadding),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("3 Sections", style: txtTheme.headlineMedium, overflow: TextOverflow.ellipsis), //headLine4
-                          Text("Programming Languages", style: txtTheme.bodyMedium, overflow: TextOverflow.ellipsis), //bodyText2
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            width: 320,
-            height: 200,
-            child: Container(
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: tCardBgColor),
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                          child: Text(
-                            "Flutter Crash Course",
-                            style: txtTheme.headlineMedium, //headLine4
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          )
-                      ),
-                      const Flexible(child: Image(image: AssetImage(tTopCourseImage1), height: 110)),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(shape: const CircleBorder()),
-                        onPressed: (){},
-                        child: const Icon(Icons.play_arrow),
-                      ),
-                      const SizedBox(width: tDashBoardCardPadding),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("3 Sections", style: txtTheme.headlineMedium, overflow: TextOverflow.ellipsis), //headLine4
-                          Text("Programming Languages", style: txtTheme.bodyMedium, overflow: TextOverflow.ellipsis), //bodyText2
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            width: 320,
-            height: 200,
-            child: Container(
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: tCardBgColor),
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                          child: Text(
-                            "Flutter Crash Course",
-                            style: txtTheme.headlineMedium, //headLine4
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          )
-                      ),
-                      const Flexible(child: Image(image: AssetImage(tTopCourseImage1), height: 110)),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(shape: const CircleBorder()),
-                        onPressed: (){},
-                        child: const Icon(Icons.play_arrow),
-                      ),
-                      const SizedBox(width: tDashBoardCardPadding),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("3 Sections", style: txtTheme.headlineMedium, overflow: TextOverflow.ellipsis), //headLine4
-                          Text("Programming Languages", style: txtTheme.bodyMedium, overflow: TextOverflow.ellipsis), //bodyText2
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ), */
         ),
       ),
     );
