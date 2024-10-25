358369bd7ac0249282e1fb4f9b925a1154dd0b6b
daniele992 <moranidaniele129@yahoo.it>
2024-10-25 22:17:36 +0200

    inserite transizioni al file app

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gestionale_calcio_mvc_flutter/src/utils/app_bindings.dart';
 import 'package:gestionale_calcio_mvc_flutter/src/utils/theme/theme.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_navigation/src/routes/transitions_type.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -15,7 +16,11 @@ class App extends StatelessWidget {
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
+
       /// -- README(Docs[4]) -- use Screen Transitions here
+      defaultTransition: Transition.leftToRightWithFade,
+      transitionDuration: const Duration(milliseconds: 500),
+
       /// -- README(Docs[5]) -- Home Screen or Progress Indicator
       home: const Scaffold(body: Center(child: CircularProgressIndicator())),
     );
