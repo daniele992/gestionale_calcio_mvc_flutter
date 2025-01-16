a582cd8f4c1f2e05ab97cf1f721e693c9714b3b6
daniele992 <moranidaniele129@yahoo.it>
2025-01-16 18:06:12 +0100

    start view LobbyScreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:gestionale_calcio_mvc_flutter/firebase_options.dart';
+import 'package:gestionale_calcio_mvc_flutter/src/features/lobby/view/lobby_screen.dart';
 import 'package:gestionale_calcio_mvc_flutter/src/repository/authentication_repository/authentication_repository.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -27,5 +28,5 @@ Future<void> main() async {
    it to the runApp function which will run the application.
   */
   runApp(const App());
-
+  //runApp(const LobbyScreen());
 }
---
+++ lib/src/features/lobby/view/lobby_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class LobbyScreen extends StatelessWidget{
+  const LobbyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+
+      ),
+    );
+  }
+}
