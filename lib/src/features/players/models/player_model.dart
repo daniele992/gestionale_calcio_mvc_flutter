625259e6195f2b965eaeb28575e2210bc33190f3
daniele992 <moranidaniele129@yahoo.it>
2025-04-28 18:43:21 +0200

    Iniziato a buttar giù le classi per i giocatori e le partite

---
+++ lib/src/features/players/models/player_model.dart
@@ -0,0 +1,33 @@
+
+
+class PlayerModel{
+  final String? id;
+  final String name;
+  final String surname;
+  final String role;
+  final String dateOfBirth;
+  final String team;
+  final String weight;
+  final String dateObservation;
+  final String foot;
+  final String height;
+
+  ///Constructor
+  const PlayerModel({
+    this.id,
+    required this.name,
+    required this.surname,
+    required this.role,
+    required this.dateOfBirth,
+    required this.team,
+    required this.weight,
+    required this.dateObservation,
+    required this.foot,
+    required this.height
+
+
+  });
+
+
+}
+
