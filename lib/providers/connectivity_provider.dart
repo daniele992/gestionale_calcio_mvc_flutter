6006cceb9d64de05571ff41c38eb546523cdf406
daniele992 <moranidaniele129@yahoo.it>
2025-06-09 16:27:24 +0200

    Aggiunti commenti

--- lib/providers/connectivity_provider.dart
+++ lib/providers/connectivity_provider.dart
@@ -2,9 +2,18 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../src/utils/connection/connectivity_service.dart';
 
-//In questo file, definiamo il provider di Riverpod che fornisce lo stato della connessione.
+//In this file, we define the Riverpod provider that provides the connection status.
+
+///Step-by-step explanation
+//1. StreamProvider<ConnectivityResult>: It exposes a stream that emits values of type ConnectivityResult.
+//2. final connectivityService = ConnectivityService(): I create an instance of a class. The connection status is being listened to
+//3. return connectivityService.connectivtyStream: The stream of the connection is returned, and it emits a new value whenever the network status changes
 
 final connectivityProvider = StreamProvider<ConnectivityResult>((ref) {
   final connectivityService = ConnectivityService();
-  return connectivityService.connectivityStream;  // Ritorna il flusso di connessione
+  return connectivityService.connectivityStream;  //Return the connection flow
 });
+
+
+
+
--- lib/src/utils/connection/connectivity_service.dart
+++ lib/src/utils/connection/connectivity_service.dart
@@ -1,8 +1,9 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 
-//Questa classe contiene la logica per verificare la connessione.
+//This class contains the logic for checking the network connection
 
 class ConnectivityService {
+
   static final ConnectivityService _instance = ConnectivityService._internal();
 
   factory ConnectivityService() {
@@ -11,16 +12,16 @@ class ConnectivityService {
 
   ConnectivityService._internal();
 
-  // Restituisce un flusso di connessione che emette una lista di ConnectivityResult
+  // It returns a stream of connection status that emits a list of ConnectivityResult values.
   Stream<ConnectivityResult> get connectivityStream {
     final connectivity = Connectivity();
     return connectivity.onConnectivityChanged.map((resultList) {
-      // Restituisce il primo elemento della lista (il più recente stato della connessione)
+      // Returns the first item in the list, which represents the most recent connection status.
       return resultList.isNotEmpty ? resultList.first : ConnectivityResult.none;
     });
   }
 
-  // Modifica per ottenere il primo valore della lista
+  // Modify to get the first value of the list.
   Future<ConnectivityResult> checkConnectivity() async {
     final connectivity = Connectivity();
     final resultList = await connectivity.checkConnectivity(); // Restituisce una lista
